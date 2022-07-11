369aa6aa5196f82d4e615f0b4f37990d24f246f3
Matias Montenegro <montenegro.matias92@gmail.com>
2022-07-11 16:42:45 -0300

    Add Sidebar

--- main.js
+++ main.js
@@ -11,14 +11,13 @@
 //     eth: 0,
 //     doge: 0,
 // }]
-
-
     //Cargamos la documentacion guardada en la billetera abajo de todo y solamente falta parsearla.
 
     let wallet = JSON.parse(localStorage.getItem('billetera'));
     let btc = JSON.parse(localStorage.getItem('cripto'));
   
 document.getElementById("wallet").innerHTML = `<p>ARS$ ${wallet}  y BTC$ ${btc}  </p>`
+document.getElementById("wallet-sidebar").innerHTML = `<p>ARS$ ${wallet}  y BTC$ ${btc}  </p>`
 
 const cryptoTrade = () =>{ //la idea a futuro es meter API para sacar el valor diario del BTC para poder hacer la conversion
 
@@ -33,6 +32,7 @@ const cryptoTrade = () =>{ //la idea a futuro es meter API para sacar el valor d
           )
     //    alert(`Perfecto, compraste ${btc} en BTC`);
         document.getElementById("wallet").innerHTML = `<p>ARS$${wallet} y BTC${btc}</p>`;
+        document.getElementById("wallet-sidebar").innerHTML = `<p>ARS$ ${wallet}  y BTC$ ${btc}  </p>`
     }
     else if(document.getElementById('amountIn').value = isNaN(amount)){
         document.getElementById('validate').innerHTML = `<p class = " p-2 text-center border border-danger rounded">X No has ingresado un monto en particular, por favor reintenta.</p>`;
@@ -54,6 +54,7 @@ moneyIn.onclick = () => { //funcion que ingresa dinero, como es infinito, sin pr
             'success'
           )
         document.getElementById("wallet").innerHTML = `<p>ARS$${wallet} y BTC${btc}</p>`;
+        document.getElementById("wallet-sidebar").innerHTML = `<p>ARS$ ${wallet}  y BTC$ ${btc}  </p>`
     }
     else if(document.getElementById('amountIn').value = isNaN(amount)){
         document.getElementById('validate').innerHTML = `<p class = " p-2 text-center border border-danger rounded">X No has ingresado un monto en particular, por favor reintenta.</p>`;
@@ -73,7 +74,7 @@ moneyOut.onclick = () =>{ //funcion que saca dinero de input para actualizar wal
             'success'
           )
         document.getElementById("wallet").innerHTML = `<p>ARS$${wallet} y BTC${btc}</p>`;
-        
+        document.getElementById("wallet-sidebar").innerHTML = `<p>ARS$ ${wallet}  y BTC$ ${btc}  </p>`
     }
     
     else if(document.getElementById('amountIn').value = isNaN(amount)){
@@ -89,3 +90,12 @@ moneyOut.onclick = () =>{ //funcion que saca dinero de input para actualizar wal
 //Local Storage, Saving All Transactions.
 let arsLoad = localStorage.setItem('billetera', JSON.parse(wallet));
 let btcLoad = localStorage.setItem('cripto', JSON.parse(btc));
+
+//Sidebar Menu Button 
+var menu_btn = document.querySelector("#menu-btn");
+var sidebar = document.querySelector("#sidebar");
+var container = document.querySelector(".my-container");
+menu_btn.addEventListener("click", () => {
+  sidebar.classList.toggle("active-nav");
+  container.classList.toggle("active-cont");
+});
